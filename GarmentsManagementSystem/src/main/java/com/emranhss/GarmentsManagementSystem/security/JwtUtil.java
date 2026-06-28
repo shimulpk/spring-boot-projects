418f9eca27ffdeb9edffe3b72b75f9ff3a07b595
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-06-28 19:04:49 +0600

    add

--- GarmentsManagementSystem/src/main/java/com/emranhss/GarmentsManagementSystem/security/JwtUtil.java
+++ GarmentsManagementSystem/src/main/java/com/emranhss/GarmentsManagementSystem/security/JwtUtil.java
@@ -1,12 +1,13 @@
 package com.emranhss.GarmentsManagementSystem.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SecretKeyAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
