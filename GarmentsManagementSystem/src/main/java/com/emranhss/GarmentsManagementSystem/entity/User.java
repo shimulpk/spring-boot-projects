33cbfd3cba98248dbae2fefbbaf93118fb765674
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-07-01 15:57:19 +0600

    add

--- GarmentsManagementSystem/src/main/java/com/emranhss/GarmentsManagementSystem/entity/User.java
+++ GarmentsManagementSystem/src/main/java/com/emranhss/GarmentsManagementSystem/entity/User.java
@@ -5,13 +5,18 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +39,9 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 }
