8cbcee70aea564bffb3f39927a2895cb5ed02476
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-07-25 13:46:51 +0600

    add

--- GarmentsManagementSystem/src/main/java/com/emranhss/GarmentsManagementSystem/controller/PurchaseOrderController.java
+++ GarmentsManagementSystem/src/main/java/com/emranhss/GarmentsManagementSystem/controller/PurchaseOrderController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/purchase-orders")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('ADMIN','PURCHASE_MANAGER')")
+@PreAuthorize("hasAnyRole('ADMIN','PURCHASE_MANAGER','STORE_MANAGER')")
 public class PurchaseOrderController {
 
     private final PurchaseOrderService purchaseOrderService;
